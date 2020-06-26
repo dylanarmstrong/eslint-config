eee063823b33624d663f5c7d7a77475a0c9736fb
Dylan Armstrong <dylan@dylan.is>
2020-06-26 09:53:27 -0500

    fix: check for plugins

--- plugins/import.js
+++ plugins/import.js
@@ -1,18 +1,32 @@
-module.exports = {
-  extends: [
-    'plugin:import/errors',
-    'plugin:import/warnings'
-  ],
-  rules: {
-    'import/no-cycle': ['error', { 'commonjs': true }],
-    'import/no-unresolved': ['error', { 'commonjs': true }]
-  },
-  settings: {
-    'import/extensions': ['.js', '.jsx', '.ts', '.tsx', '.json'],
-    'import/resolver': {
-      node: {
-        extensions: ['.js', '.jsx', '.ts', '.tsx', '.json']
+// Check for eslint-plugin-react
+let hasPlugin = true;
+try {
+  // Throws an exception when not resolvable
+  require.resolve('eslint-plugin-import');
+} catch (_) {
+  console.warn('[@dylanarmstrong/eslint] Unable to find eslint-plugin-import.');
+  hasPlugin = false;
+}
+
+if (hasPlugin) {
+  module.exports = {
+    extends: [
+      'plugin:import/errors',
+      'plugin:import/warnings'
+    ],
+    rules: {
+      'import/no-cycle': ['error', { 'commonjs': true }],
+      'import/no-unresolved': ['error', { 'commonjs': true }]
+    },
+    settings: {
+      'import/extensions': ['.js', '.jsx', '.ts', '.tsx', '.json'],
+      'import/resolver': {
+        node: {
+          extensions: ['.js', '.jsx', '.ts', '.tsx', '.json']
+        }
       }
     }
-  }
-};
+  };
+} else {
+  module.exports = {};
+}
--- plugins/react.js
+++ plugins/react.js
@@ -1,17 +1,31 @@
-module.exports = {
-  env: {
-    browser: true
-  },
-  extends: ['plugin:react/recommended'],
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true
+// Check for eslint-plugin-react
+let hasPlugin = true;
+try {
+  // Throws an exception when not resolvable
+  require.resolve('eslint-plugin-react');
+} catch (_) {
+  console.warn('[@dylanarmstrong/eslint] Unable to find eslint-plugin-react.');
+  hasPlugin = false;
+}
+
+if (hasPlugin) {
+  module.exports = {
+    env: {
+      browser: true
+    },
+    extends: ['plugin:react/recommended'],
+    parserOptions: {
+      ecmaFeatures: {
+        jsx: true
+      }
+    },
+    plugins: ['react'],
+    settings: {
+      react: {
+        version: 'detect'
+      }
     }
-  },
-  plugins: ['react'],
-  settings: {
-    react: {
-      version: 'detect'
-    }
-  }
-};
+  };
+} else {
+  module.exports = {};
+}
