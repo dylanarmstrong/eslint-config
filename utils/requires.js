25186f46b4f8ccdc1d8d365818654acf8b05a93a
Dylan Armstrong <dylan@dylan.is>
2020-11-14 09:13:45 -0600

    chore: only write once to console

--- utils/requires.js
+++ utils/requires.js
@@ -9,14 +9,22 @@ const check = (pkg) => {
   return undefined;
 };
 
+const checked = {};
+
 const requires = (name, pkgs) => {
+  if (Object.hasOwnProperty.call(checked, name)) {
+    return checked[name];
+  }
+
   const checkedPkgs = pkgs.map(check).filter(Boolean);
   if (checkedPkgs.length > 0) {
     console.warn(
       `${chalk.blue('[@dylanarmstrong/eslint]')} Disabling ${chalk.red(name)}: Unable to find ${checkedPkgs.join(', ')}.`,
     );
+    checked[name] = false;
     return false;
   }
+  checked[name] = true;
   return true;
 };
 
